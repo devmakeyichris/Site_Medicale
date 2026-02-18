bebfc1645b15e870115e55ccb731f16beeb43934
chris <devmakeyichris@gmil.com>
2026-02-18 18:06:58 +0000

    modification du fichier DocteurRepository deja optionnal

--- src/main/java/app/pfe/repository/DocteurRepository.java
+++ src/main/java/app/pfe/repository/DocteurRepository.java
@@ -1,23 +1,22 @@
 package app.pfe.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import app.pfe.entity.Docteur;
-import java.util.List;
-import java.util.Optional;
 
 
 
 @Repository
 public interface DocteurRepository extends JpaRepository<Docteur, Integer> {
 
-    boolean existByEmail(String emailDocteur);
 
-    Optional<Docteur> findDocteurByEmail(String email);
+    Optional<Docteur> findDocteurByEmailDocteur(String email);
 
-    boolean existsDocteurByEmail(String email);
+    boolean existsDocteurByEmailDocteur(String email);
 
-    boolean deleteDocteurByEmail(String email);
+    boolean deleteDocteurByEmailDocteur(String email);
     
 }
